382d5326ced7e5b8cc0fd6925c3e997073bf438e
Alexander Bleissem <alexander@bleissem.com>
2015-07-24 20:31:41 +0200

    #1 - switched to Xamarin.Forms 1.3.1.6296

--- bleissem.babyphone/bleissem.babyphone/bleissem.babyphone.Android/Resources/Resource.Designer.cs
+++ bleissem.babyphone/bleissem.babyphone/bleissem.babyphone.Android/Resources/Resource.Designer.cs
@@ -30,8 +30,6 @@ namespace bleissem.babyphone.Droid
 			global::GalaSoft.MvvmLight.Resource.String.Hello = global::bleissem.babyphone.Droid.Resource.String.Hello;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.ApplicationName = global::bleissem.babyphone.Droid.Resource.String.ApplicationName;
 			global::SQLite.Net.Platform.XamarinAndroid.Resource.String.Hello = global::bleissem.babyphone.Droid.Resource.String.Hello;
-			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::bleissem.babyphone.Droid.Resource.String.ApplicationName;
-			global::Xamarin.Forms.Platform.Resource.String.Hello = global::bleissem.babyphone.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
