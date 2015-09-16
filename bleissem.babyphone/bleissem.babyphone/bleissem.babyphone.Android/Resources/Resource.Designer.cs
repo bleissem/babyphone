d72d8d26e4774f0558f887e380d3ea72bc6060b5
Alexander Bleissem <alexander@bleissem.com>
2015-09-16 23:58:13 +0200

    #5 - style this app. yeeah

--- bleissem.babyphone/bleissem.babyphone/bleissem.babyphone.Android/Resources/Resource.Designer.cs
+++ bleissem.babyphone/bleissem.babyphone/bleissem.babyphone.Android/Resources/Resource.Designer.cs
@@ -51,6 +51,9 @@ namespace bleissem.babyphone.Droid
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int my_button = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -64,41 +67,41 @@ namespace bleissem.babyphone.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050006
-			public const int AmpTextView = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int AmpTextView = 2131099654;
 			
-			// aapt resource value: 0x7f050003
-			public const int ChooseContactButton = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int ChooseContactButton = 2131099651;
 			
-			// aapt resource value: 0x7f050009
-			public const int CloseButton = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int CloseButton = 2131099657;
 			
-			// aapt resource value: 0x7f050004
-			public const int ContactTextView = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int ContactTextView = 2131099652;
 			
-			// aapt resource value: 0x7f050000
-			public const int ContactsDetailTextView = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int ContactsDetailTextView = 2131099648;
 			
-			// aapt resource value: 0x7f050001
-			public const int ContactsList = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int ContactsList = 2131099649;
 			
-			// aapt resource value: 0x7f050005
-			public const int NoiseLevelButton = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int NoiseLevelButton = 2131099653;
 			
-			// aapt resource value: 0x7f050007
-			public const int NoiseLevelTextView = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int NoiseLevelTextView = 2131099655;
 			
-			// aapt resource value: 0x7f05000a
-			public const int PhoneNumberDetailTextView = 2131034122;
+			// aapt resource value: 0x7f06000a
+			public const int PhoneNumberDetailTextView = 2131099658;
 			
-			// aapt resource value: 0x7f05000b
-			public const int PhoneNumberList = 2131034123;
+			// aapt resource value: 0x7f06000b
+			public const int PhoneNumberList = 2131099659;
 			
-			// aapt resource value: 0x7f050008
-			public const int ServiceButton = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int ServiceButton = 2131099656;
 			
-			// aapt resource value: 0x7f050002
-			public const int textView2 = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int textView2 = 2131099650;
 			
 			static Id()
 			{
@@ -177,6 +180,28 @@ namespace bleissem.babyphone.Droid
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int my_scrollview = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int my_text_black = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int my_text_white = 2131034113;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
